d1c562f884ace858499529f79924cbda8484a4ad
Ilya Zhukevich <ilyazhukevich@gmail.com>
2022-10-07 16:07:22 +0700

    test-commit

--- src/components/user.jsx
+++ src/components/user.jsx
@@ -3,25 +3,23 @@ import Quality from "./quality";
 import BookMark from "./bookmark";
 
 const User = ({ user, handleDelete, handleToggleBookMark }) => {
+  const { name, profession, completedMeetings, rate, _id } = user;
   return (
     <>
-      <td>{user.name}</td>
+      <td>{name}</td>
       <td>
         {user.qualities.map((quality) => {
           return <Quality quality={quality} key={quality._id} />;
         })}
       </td>
-      <td>{user.profession.name}</td>
-      <td>{user.completedMeetings}</td>
-      <td>{user.rate}</td>
+      <td>{profession.name}</td>
+      <td>{completedMeetings}</td>
+      <td>{rate}</td>
       <td>
         {<BookMark user={user} handleToggleBookMark={handleToggleBookMark} />}
       </td>
       <td>
-        <button
-          className="btn btn-danger"
-          onClick={() => handleDelete(user._id)}
-        >
+        <button className="btn btn-danger" onClick={() => handleDelete(_id)}>
           Удалить
         </button>
       </td>
